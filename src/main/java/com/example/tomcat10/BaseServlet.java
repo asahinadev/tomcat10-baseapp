78930af6a6bc2be2a2df94a2f87c6a839809207e
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-04-19 19:52:40 +0900

    コネクション作成 共通化

--- src/main/java/com/example/tomcat10/BaseServlet.java
+++ src/main/java/com/example/tomcat10/BaseServlet.java
@@ -1,8 +1,15 @@
 package com.example.tomcat10;
 
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,4 +22,11 @@ public class BaseServlet extends HttpServlet {
 		super.init(config);
 	}
 
+	@SneakyThrows
+	protected Connection getConnection() {
+		Context initContext = new InitialContext();
+		Context envContext = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/example01");
+		return ds.getConnection();
+	}
 }
--- src/main/java/com/example/tomcat10/IndexServlet.java
+++ src/main/java/com/example/tomcat10/IndexServlet.java
@@ -1,12 +1,16 @@
 package com.example.tomcat10;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,10 +20,27 @@ public class IndexServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
+	@SneakyThrows
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		log.debug("{} {}", request.getMethod(), request.getRequestURI());
+
+		if (log.isDebugEnabled()) {
+			Connection con = getConnection();
+
+			PreparedStatement stm = con.prepareStatement("SHOW TABLES");
+
+			ResultSet tables = stm.executeQuery();
+
+			log.debug("TABLES -----------------------");
+			while (tables.next()) {
+				log.debug(tables.getString(1));
+			}
+			log.debug("------------------------------");
+
+		}
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/pages/index.jsp");
 		dispatcher.forward(request, response);
 
